06c0863e4323457f8e9eaa9378a45d5b02cb12a4
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-02-06 01:50:28 +0100

    Reformat code
    [skip ci]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,10 +30,12 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -79,7 +81,6 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -98,12 +99,10 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
 
-
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'onto2nxdoc'
-
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -129,10 +128,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'onto2nx.tex', u'onto2nx Documentation',
-     u'Charles Tapley Hoyt', 'manual'),
+    (master_doc, 'onto2nx.tex', u'onto2nx Documentation', u'Charles Tapley Hoyt', 'manual'),
 ]
-
 
 # -- Options for manual page output ---------------------------------------
 
@@ -143,20 +140,15 @@ man_pages = [
      [author], 1)
 ]
 
-
 # -- Options for Texinfo output -------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'onto2nx', u'onto2nx Documentation',
-     author, 'onto2nx', 'One line description of project.',
+    (master_doc, 'onto2nx', u'onto2nx Documentation', author, 'onto2nx', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
