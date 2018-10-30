60761804506f8dac46cacff1626065a5eb9d6969
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-10-30 23:10:01 +0100

    Update top level imports
    Closes #5

--- src/onto2nx/__init__.py
+++ src/onto2nx/__init__.py
@@ -1,31 +1,20 @@
 # -*- coding: utf-8 -*-
 
-"""
-onto2nx is a package for parsing ontologies in the OWL and OBO format into NetworkX graphs
-for easy traversal of entity hierarchies
-"""
+"""A package for parsing ontologies in the OWL and OBO format into NetworkX graphs."""
 
-from . import aba, api, owl_rdf, owl_xml
-from .aba import *
-from .api import *
-from .owl_rdf import *
-from .owl_xml import *
-
-__all__ = (
-    aba.__all__ +
-    owl_rdf.__all__ +
-    owl_xml.__all__ +
-    api.__all__
-)
+from .aba import aba_onto2nx
+from .api import parse_owl
+from .owl_rdf import parse_owl_rdf
+from .owl_xml import OWLParser, parse_owl_xml
 
 __version__ = '0.1.1-dev'
 
 __title__ = 'onto2nx'
-__description__ = "A package for parsing ontologies into NetworkX graphs"
+__description__ = "A package for parsing ontologies in the OWL and OBO format into NetworkX graphs"
 __url__ = 'https://github.com/cthoyt/onto2nx'
 
-__author__ = 'Charles Tapley Hoyt, Aliaksandr Masny'
-__email__ = 'charles.hoyt@scai.fraunhofer.de'
+__author__ = 'Charles Tapley Hoyt and Aliaksandr Masny'
+__email__ = 'cthoyt@gmail.com'
 
 __license__ = 'GPL 3.0 License'
 __copyright__ = 'Copyright (c) 2016-2018 Charles Tapley Hoyt'
